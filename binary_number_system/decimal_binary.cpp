5c555c0a85532f2a72168bd4480d01d44c30fc4e
Ayush Belwal <ayushbelwal261@gmail.com>
2026-04-22 20:23:08 +0530

    decimal to binary conversion

---
+++ binary_number_system/decimal_binary.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dec_bin(int decNum){
+    int pow = 1 , ans = 0 ;
+    while (decNum > 0){
+        int rem = decNum%2 ;
+        decNum = decNum/2 ;
+        ans = ans + (rem*pow) ;
+        pow = pow * 10 ;
+    }
+    return ans;
+}
+
+int main(){
+    cout << dec_bin(50);
+    return 0;
+}
